79ce6abdea4ff7dffdcc8c170bba9cdf3bdbd694
Jakub Martyński <69093918+XPenguin2002@users.noreply.github.com>
2024-03-10 18:56:30 +0100

    Add files via upload

---
+++ chat-gpt-bot/index.js
@@ -0,0 +1,90 @@
+process.chdir(__dirname);
+require('dotenv/config');
+const { Client } = require('discord.js');
+const { OpenAI } = require('openai');
+
+const client = new Client({
+    intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'],
+});
+
+client.on('ready', () => {
+    console.log('The bot is online.');
+});
+
+const IGNORE_PREFIX = "/";
+const CHANNELS = ['1170813054175412334','1170818701860294718'];
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY,
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(IGNORE_PREFIX)) return;
+    if (!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) 
+        return;
+
+    await message.channel.sendTyping();
+
+    const sendTypingInterval = setInterval(() => {
+        message.channel.sendTyping();
+    }, 5000);
+
+    let conversation = [];
+
+    conversation.push({
+        role: 'system',
+        content: 'Penguin GPT tak o',
+    });
+
+    let prevMessages = await message.channel.messages.fetch({ limit: 10 });
+    prevMessages.reverse();
+
+    prevMessages.forEach((msg) => {
+        if (msg.author.bot && msg.author.id !== client.user.id) return;
+        if (msg.content.startsWith(IGNORE_PREFIX)) return;
+
+        const username = msg.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, '');
+
+        if (msg.author.is === client.user.id) {
+            conversation.push({
+                role: 'assistant',
+                name: username,
+                content: msg.content,
+            });
+
+            return;
+        }
+
+        conversation.push({
+            role: 'user',
+            name: username,
+            content: msg.content,
+        })
+    });
+
+    const response = await openai.chat.completions
+    .create({
+        model: 'gpt-4',
+        messages: conversation,
+    })
+    .catch((error) => console.error('OpenAI Error:\n', error));
+
+    clearInterval(sendTypingInterval);
+
+    if (!response) {
+        message.reply("Mam pewne problemy z API OpenAI. Spróbuj ponownie za chwilę.");
+        return;
+    }
+
+    const responseMessage = response.choices[0].message.content;
+    const chunkSizeLimit = 2000;
+
+    for (let i = 0; i < responseMessage.length; i += chunkSizeLimit) {
+        const chunk = responseMessage.substring(i, i + chunkSizeLimit);
+
+        await message.reply(chunk);
+    }
+});
+
+client.login(process.env.TOKEN);
